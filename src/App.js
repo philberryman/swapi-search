56d5a9f26f3d0e2aaaefab8262efac72e3d05191
Phil B <phil@berryman.org.uk>
2019-03-12 07:56:13 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Downshift from "downshift";
+
+const items = [
+  { fruit: true, value: "apple" },
+  { fruit: true, value: "pear" },
+  { fruit: true, value: "orange" },
+  { fruit: true, value: "grape" },
+  { fruit: true, value: "banana" },
+  { fruit: false, value: "apple" },
+  { fruit: false, value: "pear" },
+  { fruit: false, value: "orange" },
+  { fruit: false, value: "grape" },
+  { fruit: false, value: "banana" },
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Downshift
+        onChange={selection => alert(`You selected ${selection.value}`)}
+        itemToString={item => (item ? item.value : "")}
+      >
+        {({
+          getInputProps,
+          getItemProps,
+          getLabelProps,
+          isOpen,
+          inputValue,
+          highlightedIndex,
+          selectedItem,
+        }) => (
+          <div>
+            <label {...getLabelProps()}>Enter a fruit</label>
+            <input {...getInputProps()} />
+            {isOpen ? (
+              <div>
+                {items
+                  .filter(
+                    item => !inputValue || item.value.includes(inputValue)
+                  )
+                  .map((item, index) => (
+                    <div>
+                      <div>{item.fruit ? "fruit" : "veg"}</div>
+                      <div
+                        {...getItemProps({
+                          key: item.value,
+                          index,
+                          item,
+                          style: {
+                            backgroundColor:
+                              highlightedIndex === index
+                                ? "lightgray"
+                                : "white",
+                            fontWeight:
+                              selectedItem === item ? "bold" : "normal",
+                          },
+                        })}
+                      >
+                        {item.value}
+                      </div>
+                    </div>
+                  ))}
+              </div>
+            ) : null}
+          </div>
+        )}
+      </Downshift>
     );
   }
 }
